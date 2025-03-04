3e6f65a01095e2761ef78df48b9779a76bc6b064
andrerezendebr <andrerezende_br@yahoo.com.br>
2025-03-04 14:20:53 -0300

    Reorganização e limpeza do código python e javascrip.

--- src/backend/webdir/AutiTest.py
+++ src/backend/webdir/AutiTest.py
@@ -8,7 +8,4 @@ longitude = -47.8822  # Exemplo de longitude (Brasília)
 altitude = wr.getElevationOpenElev(latitude, longitude)
 print(f"Altitude: {altitude} metros")
 
-
-# wr.generate_elevation_table_png(output_filename="img.png",min_elevation=61,max_elevation=651)  
-# wr.generate_elevation_table_png(output_filename="img.png",min_elevation=0,max_elevation=651)  
 ###########################################################################################################################
