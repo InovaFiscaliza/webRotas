fa3d221275845c27a2f0395636e72ea7efe16e3f
lobao <fabioslobao@hotmail.com>
2025-03-13 11:19:50 +0100

    Update map referente to 2023

--- src/backend/webdir/shapeFiles.py
+++ src/backend/webdir/shapeFiles.py
@@ -5,50 +5,56 @@ import matplotlib.pyplot as plt
 from shapely.geometry import box
 from shapely.geometry import Polygon, MultiPolygon
 
+
 ###########################################################################################################################
-# https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2022/Brasil/BR/BR_Municipios_2022.zip
+# https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2023/Brasil/BR_Municipios_2023.zip
 ##########################################################################################################################
 def GetBoundMunicipio(nome_municipio, estado_sigla):
     """
     Função para obter o limite geográfico e o centroide de um município específico e retornar a Polyline.
-    
+
     Parâmetros:
         nome_municipio (str): Nome do município.
         estado_sigla (str): Sigla do estado (ex: 'RJ' para Rio de Janeiro).
-        
+
     Retorna:
         polyline (list): Lista de coordenadas [(lat, lon), ...] representando o limite do município.
         centroide (Point): Coordenadas do centroide do município.
     """
     wr.wLog(f"GetBoundMunicipio - {nome_municipio} - {estado_sigla}")
-    # Carregar o arquivo Shapefile BR_Municipios_2022.shp
-    shapefile_path = '../../resources/BR_Municipios_2022/BR_Municipios_2022.shp'
+
+    # Carregar o arquivo Shapefile BR_Municipios_2023.shp
+    shapefile_path = "../../resources/BR_Municipios/BR_Municipios_2023.shp"
     gdf = gpd.read_file(shapefile_path)
-    
+
     # Filtrar município e estado
-    municipio = gdf[(gdf['NM_MUN'] == nome_municipio) & (gdf['SIGLA_UF'] == estado_sigla)]
+    municipio = gdf[
+        (gdf["NM_MUN"] == nome_municipio) & (gdf["SIGLA_UF"] == estado_sigla)
+    ]
 
     if municipio.empty:
-        wr.wLog(f"Município '{nome_municipio}' no estado '{estado_sigla}' não encontrado.")
+        wr.wLog(
+            f"Município '{nome_municipio}' no estado '{estado_sigla}' não encontrado."
+        )
         return None
-    
+
     # Obter a geometria do município
     geometria = municipio.geometry.values[0]  # Geometria do limite
-    centroide = geometria.centroid            # Centroide do município
-    
+    centroide = geometria.centroid  # Centroide do município
+
     # Extrair coordenadas como Polyline
     polyline = []
-    if geometria.geom_type == 'MultiPolygon':
+    if geometria.geom_type == "MultiPolygon":
         # Se for MultiPolygon, concatenar coordenadas de todos os polígonos
         wr.wLog("Foi multipoligon - Cidade possui ilhas ou áreas isoladas")
-        
+
         for polygon in geometria.geoms:
             polyline.append(list(polygon.exterior.coords))
     else:
         # Se for Polygon, apenas extrair as coordenadas
         polyline.append(list(geometria.exterior.coords))
-    
-    # Retornar a Polyline (como lista de coordenadas) 
+
+    # Retornar a Polyline (como lista de coordenadas)
     return polyline
 
 
@@ -94,12 +100,13 @@ def FiltrarComunidadesBoundingBox(bounding_box):
         elif isinstance(geom, MultiPolygon):
             for poly in geom.geoms:  # Acessar os polígonos do MultiPolygon
                 polylines.append([(lat, lon) for lon, lat in poly.exterior.coords])
-    
+
     # Imprimir a lista de polylines
-    #for i, polyline in enumerate(polylines):
+    # for i, polyline in enumerate(polylines):
     #    print(f"Polyline {i+1}: {polyline}")
-        
+
     return polylines
+
 
 ##############################################################################################################
 def main():
@@ -107,18 +114,19 @@ def main():
     Função principal para executar o script.
     """
 
-
     # Definir os limites do bounding box para Niterói (RJ)
     bounding_box = (-43.1363, -22.9488, -43.0329, -22.8708)
 
     # Chamar a função para filtrar e plotar
-    dados=FiltrarComunidadesBoundingBox(bounding_box)
-    
-     # Plotar os dados filtrados
+    dados = FiltrarComunidadesBoundingBox(bounding_box)
+
+    # Plotar os dados filtrados
     fig, ax = plt.subplots(1, 1, figsize=(8, 8))
     dados.plot(ax=ax, color="blue", edgecolor="black")
     plt.title("Área Filtrada")
-    plt.show()   
+    plt.show()
+
+
 ##############################################################################################################
 if __name__ == "__main__":
     main()
