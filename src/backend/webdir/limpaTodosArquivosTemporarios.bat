070398797e3ffccae3b9b6fa05bd5f94386c5b50
andrerezendebr <andrerezende_br@yahoo.com.br>
2025-03-25 18:57:02 -0300

    Cleaning scripts issues solved

--- src/backend/webdir/limpaTodosArquivosTemporarios.bat
+++ src/backend/webdir/limpaTodosArquivosTemporarios.bat
@@ -2,5 +2,5 @@ taskkill /F /IM python.exe /T
 FOR /F "tokens=*" %%i IN ('podman ps -a -q') DO podman stop %%i
 wsl rm -rf logs/Web*
 wsl rm -rf ../../resources/Osmosis/TempData/exclu*
-wsl rm -rf ../../resources/OSMR/data/TempData/filtro_*
-wsl rm -rf templates/Mapa*
+wsl rm -rf ../../resources/OSMR/data/TempData/filtro*
+wsl rm -rf templates/WebRotas*
--- src/ucli/limpa.bat
+++ src/ucli/limpa.bat
@@ -1,6 +1,7 @@
 taskkill /F /IM python.exe /T
 FOR /F "tokens=*" %%i IN ('podman ps -a -q') DO podman stop %%i
-wsl rm -rf "%WEBROTAS_HOME%\src\backend\webdir\logs\Web*"
-wsl rm -rf "%WEBROTAS_HOME%\src\resources\Osmosis\TempData\exclu*"
-wsl rm -rf "%WEBROTAS_HOME%\src\resources\OSMR\data\TempData\filtro_*"
-wsl rm -rf "%WEBROTAS_HOME%\src\backend\webdir\templates\Mapa*"
+del /Q /S "%WEBROTAS_HOME%\src\backend\webdir\logs\Web*"
+del /Q /S "%WEBROTAS_HOME%\src\resources\Osmosis\TempData\exclu*"
+del /Q /S "%WEBROTAS_HOME%\src\resources\OSMR\data\TempData\filtro*"
+for /d %%D in ("%WEBROTAS_HOME%\src\resources\OSMR\data\TempData\filtro*") do rmdir /S /Q "%%D"
+del /Q /S "%WEBROTAS_HOME%\src\backend\webdir\templates\WebRotas*"
