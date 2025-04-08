f79d585b0987fa0bd26069a956405380d3a08def
andrerezendebr <andrerezende_br@yahoo.com.br>
2025-04-08 09:40:39 -0300

    New Examples

---
+++ tests/runselected.bat
@@ -0,0 +1,9 @@
+cd ..
+set WEBROTAS_HOME=%CD%
+cd tests
+
+uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_abrangencia_rj_niteroi_geodesica.json
+uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_contorno.json
+uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_visita_ro.json
+uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_visita_sp_cidades_interior.json
+
