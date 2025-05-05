762e7785184f2f6f819b47b91b99c88b905c70d6
andrerezendebr <andrerezende_br@yahoo.com.br>
2025-05-05 19:51:47 -0300

    Urban Areas caching development

--- src/ucli/limpa.bat
+++ src/ucli/limpa.bat
@@ -2,6 +2,4 @@ taskkill /F /IM python.exe /T
 FOR /F "tokens=*" %%i IN ('podman ps -a -q') DO podman stop %%i
 del /Q /S "%WEBROTAS_HOME%\src\backend\webdir\logs\Web*"
 del /Q /S "%WEBROTAS_HOME%\src\resources\Osmosis\TempData\exclu*"
-del /Q /S "%WEBROTAS_HOME%\src\resources\OSMR\data\TempData\filtro*"
-for /d %%D in ("%WEBROTAS_HOME%\src\resources\OSMR\data\TempData\filtro*") do rmdir /S /Q "%%D"
 del /Q /S "%WEBROTAS_HOME%\src\backend\webdir\templates\WebRotas*"
