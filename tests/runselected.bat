d6dbbfe791e0fd26e92e161cd69dbd136204376c
andrerezendebr <andrerezende_br@yahoo.com.br>
2025-07-23 22:00:56 -0300

    Use management for routes without online server demanded from javascrip webinterface.

--- tests/runselected.bat
+++ tests/runselected.bat
@@ -2,8 +2,8 @@ cd ..
 set WEBROTAS_HOME=%CD%
 cd tests
 
-rem uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_contorno.json
-rem uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_abrangencia_belem.json
+uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_contorno.json
+uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_abrangencia_belem.json
 uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_visita_ro.json
 
 rem uv run --project "%WEBROTAS_HOME%" "%WEBROTAS_HOME%\src\ucli\webrota_client.py" exemplo_visita_portoalegre-belem.json
